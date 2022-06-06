971c9453c05f7dfb59dd9e88a265089490e6a336
selfstudy1 <102909589+qtest3@users.noreply.github.com>
2022-06-06 20:20:32 +0800

    2

--- dev.go
+++ dev.go
@@ -4,7 +4,7 @@ import (
      //青龙2.9机器人
 	 _ "github.com/qtest3/young1_go_ql"
 	 //京东账号
-	 _ "github.com/qtest3/young1_go_jd"
+	//  _ "github.com/qtest3/young1_go_jd"
 	 //腾讯QQ
 	 _ "github.com/qtest3/young1_go_silly/im/qq"
 	 //telegram
